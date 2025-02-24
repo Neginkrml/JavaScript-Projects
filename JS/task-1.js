a1c08f281f98112e0d07f27123c8e689c6cfabd2
neginkerim <ngnkrml03@gmail.com>
2025-02-24 13:53:32 +0300

    To-Do list

---
+++ JS/task-1.js
@@ -0,0 +1,55 @@
+const inputBox = document.querySelector(".input-box")
+const listContainer = document.querySelector(".list-container")
+
+// function addTask() {
+//     if (inputBox.value === "") {
+//         alert("You must write something!");
+//     } else {
+//         let li = document.createElement("li"); 
+
+//         li.innerHTML = inputBox.value;
+//         listContainer.appendChild(li);
+//     }
+//     inputBox.value = "";
+// }
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        let icon = document.createElement("i");
+        icon.classList.add("fa-regular", "fa-circle-check");
+
+        li.appendChild(icon);
+        li.appendChild(document.createTextNode(" " + inputBox.value));
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+};
+showTask();
